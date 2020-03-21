6465054c7a8d173723a50d4b75121d9dfeed4f8d
Denis Angell <denisangell@harpangell.com>
2020-03-21 14:24:35 -0400

    Add Base64/Array Functions

--- static/src/utils/misc.js
+++ static/src/utils/misc.js
@@ -92,3 +92,23 @@ export function validateLocation(location) {
     }
     return true
 }
+
+export function base64ToArrayBuffer(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
+export function arrayBufferToBase64(buffer) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+}
