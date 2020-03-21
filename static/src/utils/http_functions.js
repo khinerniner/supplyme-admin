decb76fc02930ac6bb47f317770582e92ce3574c
Denis Angell <denisangell@harpangell.com>
2020-03-21 14:24:42 -0400

    Add Google HTTP Functions

--- static/src/utils/http_functions.js
+++ static/src/utils/http_functions.js
@@ -14,3 +14,32 @@ const dataUploadConfig = token => ({
         'content-type': 'multipart/form-data',
     },
 });
+
+// Search Google Places
+// TODO: None
+// [START Search Google Places]
+export function apiSearchGooglePlaces(query, type) {
+    return axios.get('/api/google/v1/places/search', {
+        params: {
+            query,
+            type,
+        },
+    });
+}
+// [END Search Google Places]
+
+// Geocode Google Place
+// TODO: None
+// [START Geocode Google Place]
+export function apiGeocodeGooglePlace(token, accountID, place) {
+    return axios.get('/api/google/v1/places/geocode', {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        params: {
+            accountID,
+            place,
+        },
+    });
+}
+// [END Geocode Google Place]
