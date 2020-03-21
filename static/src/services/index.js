fcaf60f304c6ccaf358e1be1dbd6a1e2de50bdcc
Denis Angell <denisangell@harpangell.com>
2020-03-21 14:28:06 -0400

    Update Index Reducer Store

--- static/src/services/index.js
+++ static/src/services/index.js
@@ -5,11 +5,17 @@ import { LOGOUT_USER } from '../constants/index';
 /* Admin Reducer Files */
 import app from './app/reducer';
 import accountData from './account/reducer';
+import hospitalData from './hospital/reducer';
+import employeeData from './employee/reducer';
+import googleData from './google/reducer';
 
 const appReducer = history => combineReducers({
     router: connectRouter(history),
     app,
     accountData,
+    hospitalData,
+    employeeData,
+    googleData,
 });
 
 export const rootReducer = history => (state, action) => {
