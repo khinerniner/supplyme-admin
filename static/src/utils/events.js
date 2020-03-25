d427961bcf787e567ac0c334c1783f9116a97cf8
Denis Angell <denisangell@harpangell.com>
2020-03-25 15:09:15 -0400

    Add Status Event Parse Function

---
+++ static/src/utils/events.js
@@ -0,0 +1,25 @@
+export function formatRequestStatus(status) {
+    switch (status) {
+    case 0:
+        return 'Created';
+    case 1:
+        return 'Submitted';
+    case 2:
+        return 'Funding Approved';
+    case 3:
+        return 'Funding Denied';
+    case 4:
+        return 'In Progress';
+    case 5:
+        return 'Shipped';
+    case 6:
+        return 'Delivered';
+    case 7:
+        return 'Completed';
+    case 8:
+        return 'Deleted';
+
+    default:
+        return 'Created';
+    }
+}
