8b3d3e63467bd2bce8d4a1dd94fe0e4016ab92b0
Denis Angell <denisangell@harpangell.com>
2020-03-25 18:03:01 -0400

    Add Order Status Events

--- static/src/utils/events.js
+++ static/src/utils/events.js
@@ -23,3 +23,29 @@ export function formatRequestStatus(status) {
         return 'Created';
     }
 }
+
+export function formatOrderStatus(status) {
+    switch (status) {
+    case 0:
+        return 'Created';
+    case 1:
+        return 'Submitted';
+    case 2:
+        return 'Approved';
+    case 3:
+        return 'Denied';
+    case 4:
+        return 'In Progress';
+    case 5:
+        return 'Shipped';
+    case 6:
+        return 'Delivered';
+    case 7:
+        return 'Completed';
+    case 8:
+        return 'Deleted';
+
+    default:
+        return 'Created';
+    }
+}
