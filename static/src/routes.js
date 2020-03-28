0d5accfa494f8dd117ea3351b0d122bd7d65c72c
Denis Angell <denisangell@harpangell.com>
2020-03-28 15:23:02 -0400

    Add Employee Routes

--- static/src/routes.js
+++ static/src/routes.js
@@ -9,9 +9,14 @@ import App from './containers/App';
 import NotFoundView from './containers/Global/NotFoundView';
 import LoginView from './containers/NotAuth/Register/LoginView';
 import AccountRegisterView from './containers/NotAuth/Register/AccountRegisterView';
-
 import ValorListView from './containers/NotAuth/Valor/ValorListView';
 import ValorCreateView from './containers/NotAuth/Valor/ValorCreateView';
+
+/* Private Containers */
+import EmployeeListView from './containers/VeriDoc/Employee/EmployeeListView';
+import EmployeeDetailView from './containers/VeriDoc/Employee/EmployeeDetailView';
+import EmployeeCodeListView from './containers/VeriDoc/Employee/EmployeeCodeListView';
+import EmployeeCodeCreateView from './containers/VeriDoc/Employee/EmployeeCodeCreateView';
 import LocationListView from './containers/VeriDoc/Location/LocationListView';
 import LocationCreateView from './containers/VeriDoc/Location/LocationCreateView';
 import RequestListView from './containers/VeriDoc/Request/RequestListView';
@@ -30,13 +35,15 @@ export default (
     <App>
         <Switch>
             // Auth Views
-            <Route exact path="/" component={requireAuthentication(ValorListView)} />
+            <Route exact path="/" component={requireAuthentication(LocationListView)} />
             <Route exact path="/register" component={requireNoAuthentication(AccountRegisterView)} />
             <Route exact path="/login" component={requireNoAuthentication(LoginView)} />
-            <Route exact path="/valor" component={requireNoAuthentication(ValorListView)} />
-            <Route exact path="/valor/create" component={requireNoAuthentication(ValorCreateView)} />
 
             <Route exact path="/accounts/:id/account" component={requireAuthentication(LocationListView)} />
+            <Route exact path="/accounts/:id/employees" component={requireAuthentication(EmployeeListView)} />
+            <Route exact path="/accounts/:id/employees/codes" component={requireAuthentication(EmployeeCodeListView)} />
+            <Route exact path="/accounts/:id/employees/:id" component={requireAuthentication(EmployeeDetailView)} />
+            <Route exact path="/accounts/:id/employees/codes/create" component={requireAuthentication(EmployeeCodeCreateView)} />
             <Route exact path="/accounts/:id/locations" component={requireAuthentication(LocationListView)} />
             <Route exact path="/accounts/:id/locations/create" component={requireAuthentication(LocationCreateView)} />
             <Route exact path="/accounts/:id/requests" component={requireAuthentication(RequestListView)} />
@@ -46,6 +53,9 @@ export default (
             <Route exact path="/accounts/:id/orders" component={requireAuthentication(OrderListView)} />
             <Route exact path="/accounts/:id/orders/search" component={requireAuthentication(PublicRequestListView)} />
             <Route exact path="/accounts/:id/orders/create/requests/:id" component={requireAuthentication(OrderCreateView)} />
+
+            <Route exact path="/valor" component={requireNoAuthentication(ValorListView)} />
+            <Route exact path="/valor/create" component={requireNoAuthentication(ValorCreateView)} />
             <Route component={NotFoundView} />
         </Switch>
     </App>
