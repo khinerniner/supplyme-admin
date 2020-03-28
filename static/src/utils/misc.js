aa713a84b6fd5e8e4ffb561cb2a9e680daf94a15
Denis Angell <denisangell@harpangell.com>
2020-03-28 15:22:31 -0400

    Add Parse Time Function

--- static/src/utils/misc.js
+++ static/src/utils/misc.js
@@ -13,6 +13,27 @@ Time Functions
 
 */
 
+export function formatDateWTime(date) {
+    if (!validateDate(date)) {
+        return 'Invalid Date';
+    }
+    // console.log(date);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    var monthNames = [
+        "Jan", "Feb", "March",
+        "April", "May", "June", "July",
+        "Aug", "Sept", "Oct",
+        "Nov", "Dec"
+    ];
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    hours %= 12;
+    hours = hours || 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    const strTime = `${hours}:${minutes} ${ampm}`;
+    return `${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}  ${strTime}`;
+}
+
 export function formatDateNoTime(date) {
     if (!validateDate(date)) {
         return 'Invalid Date';
