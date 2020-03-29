80eca4c61a769477fb88e7d207571ffb56244f50
Denis Angell <denisangell@harpangell.com>
2020-03-29 04:50:42 -0400

    New MenuItem Model v2

--- static/src/services/menuItem/model.js
+++ static/src/services/menuItem/model.js
@@ -1,5 +1,66 @@
 import { parseFirestoreTimeStamp } from '../../utils/misc';
 
+
+/*
+Quantites
+
+Linear Measure
+
+in
+ft
+yd
+rd
+fur
+mi
+league
+statute
+nauticalMile
+
+Area Measure
+
+sqft
+sqyd
+sqrd
+acre
+sqmi
+section
+township
+
+Cubic Measure
+
+cuft
+cuyd
+
+Liquid Measure
+
+gill
+pint
+quart
+gallog
+
+Avoirdupois Weight
+
+grain
+dr: dram
+oz
+lb
+cwt: hunderedweight
+ton
+grosscwt: gross hunderedweight
+
+Other
+ea: Each
+
+*/
+
+/*
+
+Package Type
+piece
+weight
+
+*/
+
 export function getMenuItemFromSnapshot(menuItem) {
     return {
         itemID: menuItem.itemID,
@@ -7,20 +68,29 @@ export function getMenuItemFromSnapshot(menuItem) {
         active: menuItem.active,
         deleted: menuItem.deleted,
         itemID: menuItem.itemID,
-        skuID: menuItem.skuID,
-        certID: menuItem.certID,
+        upcID: menuItem.upcID,
         brandName: menuItem.brandName,
+        // tagLine: menuItem.tagLine,
+        // manufacturerName: menuItem.manufacturerName,
         itemName: menuItem.itemName,
         itemType: menuItem.itemType,
         locationID: menuItem.locationID,
-        notes: menuItem.notes,
+        description: menuItem.description,
         oldItemRef: menuItem.oldItemRef,
         quantities: menuItem.quantities,
+        // measurement: menuItem.measurement,
+        // lwd: menuItem.lwd,
         thumbItemImageURL: menuItem.thumbItemImageURL,
         updatedDate: parseFirestoreTimeStamp(menuItem.updatedDate),
         createdDate: parseFirestoreTimeStamp(menuItem.createdDate),
         availableStockPerItem: menuItem.availableStockPerItem,
         unassignedStockPerItem: menuItem.unassignedStockPerItem,
+        // supplies: menuItem.supplies,
+        // supportingDocs: menuItem.supportingDocs,
+        // isFinished: menuItem.isFinished,
+        // private: menuItem.private,
+        // warningLabel: menuItem.warningLabel,
+        madeInCountry: menuItem.madeInCountry,
     };
 }
 export function toNewMenuItem() {
@@ -30,24 +100,42 @@ export function toNewMenuItem() {
         active: false,
         deleted: false,
         itemID: null,
-        skuID: null,
-        certID: null,
+        upcID: null,
         brandName: null,
+        // tagLiacturerName: null,
         itemName: null,
         itemType: 'ppe',
         locationID: null,
-        notes: null,
+        description: null,
         oldItemRef: null,
         quantities: [],
+        // measurement: {
+        //     label: null,
+        //     units: 0,
+        // },
+        // lwd: {
+        //     length: 0,
+        //     width: 0,
+        //     depth: 0,
+        // },
         thumbItemImageURL: null,
         updatedDate: null,
         createdDate: null,
         availableStockPerItem: {},
         unassignedStockPerItem: {},
+        // supplies: [],
+        // supportingDocs: [],
+        // isFinished: false,
+        // private: false,
+        // warningLabel: null,
+        madeInCountry: 'USA',
     };
 }
 export function toNewQuantity() {
     return {
+        packageQuantity: 1,
+        packageType: 'piece',
+        weightType: 'oz',
         pricePerUnit: 0,
         stock: 0,
     };
@@ -59,9 +147,10 @@ export function menuItemRowObject(menuItem) {
         active: menuItem.active,
         deleted: menuItem.deleted,
         brandName: menuItem.brandName,
-        skuID: menuItem.skuID,
+        upcID: menuItem.upcID,
         itemName: menuItem.itemName,
         itemType: menuItem.itemType,
+        madeInCountry: menuItem.madeInCountry,
         updatedDate: menuItem.updatedDate,
         createdDate: menuItem.createdDate,
     };
