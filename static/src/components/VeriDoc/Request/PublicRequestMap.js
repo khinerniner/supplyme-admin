167d598ebe31799b270121b5e642f422ffe5a1be
Denis Angell <denisangell@harpangell.com>
2020-03-28 23:08:08 -0400

    Add Map & Map View (Requests)

---
+++ static/src/components/VeriDoc/Request/PublicRequestMap.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { MarkerClusterer } from "react-google-maps/lib/components/addons/MarkerClusterer";
+
+import TablePaginationActions from '../../TablePaginationActions';
+
+import {
+  formatDateWTime,
+  formatAddress,
+  formatDateNoTime
+} from '../../../utils/misc';
+import {
+  formatRequestStatus
+} from '../../../utils/events';
+
+const styles = (theme) => ({
+  root: {
+    width: '100%',
+    marginTop: 40,
+    overflowX: 'auto',
+    boxShadow: 'none',
+  },
+  table: {
+    minWidth: 650,
+    overflow: 'hidden',
+  },
+  tableHeaders: {
+    fontSize: 12,
+    fontWeight: 500,
+    borderBottom: '1px solid #d6d6d6',
+    borderLeft: 0,
+    verticalAlign: 'bottom',
+    color: theme.palette.primary.black,
+  },
+  linkText: {
+    color: '#2A38D8',
+    fontWeight: 500,
+    cursor: 'pointer',
+    textAlign: 'left',
+  },
+  cancelIcon: {
+    color: '#e02626',
+    margin: 0,
+    padding: 0,
+  },
+  checkIcon: {
+    color: '#37e026',
+    margin: 0,
+    padding: 0,
+  },
+  mapStyles: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+const PublicRequestMap = withScriptjs(withGoogleMap((props) => {
+  const { classes, isMarkerShown, markers, currentCoords } = props;
+  console.log(markers);
+  return (
+    <Paper className={classes.root}>
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={currentCoords}
+        >
+            {
+              <MarkerClusterer
+                // onClick={props.onMarkerClustererClick}
+                averageCenter
+                enableRetinaIcons
+                gridSize={60}
+              >
+                {markers.map(marker => (
+                  <Marker
+                    key={marker.requestID}
+                    position={{ lat: marker.latitude, lng: marker.longitude }}
+                    icon={marker.img}
+                  />
+                ))}
+              </MarkerClusterer>
+            }
+        </GoogleMap>
+    </Paper>
+  );
+}));
+
+
+
+PublicRequestMap.propTypes = {
+  isMarkerShown: PropTypes.bool.isRequired,
+  googleMapURL: PropTypes.string.isRequired,
+  loadingElement: PropTypes.object.isRequired,
+  containerElement: PropTypes.object.isRequired,
+  mapElement: PropTypes.object.isRequired,
+  markers: PropTypes.array.isRequired,
+  handleLink: PropTypes.func.isRequired,
+  handleAction: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(PublicRequestMap);
