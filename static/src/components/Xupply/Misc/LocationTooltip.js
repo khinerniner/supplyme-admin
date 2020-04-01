cef1c35859c38e60a02ffbee37a8092dd851e13e
Denis Angell <denisangell@harpangell.com>
2020-03-31 22:29:09 -0400

    Create Seperate Component For Location Tool Tip

---
+++ static/src/components/Xupply/Misc/LocationTooltip.js
@@ -0,0 +1,13 @@
+import { withStyles } from '@material-ui/core/styles';
+
+const LocationTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}))(Tooltip);
+
+export default LocationTooltip;
