8ca8ebff81fec8174113afd30733a9442f0f8125
Denis Angell <denisangell@harpangell.com>
2020-04-02 04:34:32 -0400

    Remove Old Code

--- static/src/utils/analytics.js
+++ static/src/utils/analytics.js
@@ -1,14 +1,5 @@
 
 // Event is (action_model_result)
-export function generalAnalytic(userId, event, eventCategory) {
-    console.log(eventCategory);
-    return gtag(
-        'event', event, {
-            event_category: eventCategory,
-        },
-    );
-}
-
 export function xupplyAnalytic(event, parameters) {
     return gtag(
         'event', event, {
